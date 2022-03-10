d018934fb77ee41480c43c3571aaeea9d9f55836
Ismael Zanati <ismael.z2@gso.schule.koeln>
2022-03-10 10:54:30 +0100

    Projektdateien hinzufügen.

---
+++ Convert Tolnt/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Convert_Tolnt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World");
+            string aFriend = "Bill";
+            Console.WriteLine(aFriend);
+            Console.WriteLine("Hello" + aFriend);
+            String firstfriend = "Ismael";
+            string secondfriend = "Mohammed";
+            Console.WriteLine($"My friends are {firstfriend} and {secondfriend}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
